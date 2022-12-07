6a035d8c94e0a3bbf418d741744e8f935e9165d6
EverGreenA <113111542+EverGreenA@users.noreply.github.com>
2022-12-07 20:30:27 +0900

    초기 커밋.

---
+++ Project1/소스.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	
+	int i;
+	int receive;
+	int j;
+	int k;
+	int num = 0;
+
+	scanf_s("%d", &receive);
+
+
+	for (i = 0; i < receive; i++) {
+		for (j = 1; j < receive - i; j++) {
+			printf(" ");
+		}
+		for (k = 0; k <= i*2; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
